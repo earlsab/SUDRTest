7be3da51c01bb39b6f89fe79c0a8aa709f292acb
ChuuniQ <kyleestabillo@su.edu.ph>
2022-09-21 01:08:22 +0800

    - Fixed layouts and added important comments. enter the commit message for your changes. Lines starting

--- resources/views/papers/viewPDFAdmin.blade.php
+++ resources/views/papers/viewPDFAdmin.blade.php
@@ -6,6 +6,23 @@
         <h1 class="papersheading">View PDF</h1>
 			<hr class="modline">
 
+            <div class="paperinfoblock">
+
+                <div class="paperinfo">
+                    <h2>
+                        <!-- is it possible make this into the paper title? -->
+                        <span class="dispfullname"><b>{{ Auth::user()->name }}</b></span> 
+                    </h2>
+                </div>
+
+                <div class="paperinfo">
+                    <span>Author(s): </span>
+                    <!-- is it possible make this into the author of the paper? -->
+                    <span class="dispemail">asdasdasd</span>
+                </div>
+                
+            </div>
+
             <iframe  class="pdfviewer" src="/assets/{{$data->file}}"></iframe>
 
 </div>
