d45163a0c5ae46410c704edd1e60ddfe584e29bb
ChuuniQ <kyleestabillo@su.edu.ph>
2022-11-20 22:24:38 +0800

    - added pdf viewer

--- resources/views/papers/viewPDF.blade.php
+++ resources/views/papers/viewPDF.blade.php
@@ -2,53 +2,121 @@
 
 @section('content')
 
-<div class="mypapersblock">
-        <h1 class="papersheading"><b></b></h1>	        
-        <hr class="modline">
 
-        
-       <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addBookmark">
-  Bookmark This
-</button>
+<div class="viewPDFCont">
 
-<!-- Modal -->
-<div class="modal fade" id="addBookmark" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Add Bookmark</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form action="{{route('Bookmarks')}}" method="POST">
-                {{csrf_field()}}
-                <div class="form-group">
-                        <label>Bookmark Name</label>
-                        <input type="text" class="form-control" name="BookmarkName">
-                        <input type="hidden" class="form-control" name="paper_id" value="">
+    <div class="pdfinfoDisplay">
+
+        <div class="pdfinfoCard">
+
+            <li class="paperinfoHeader">
+                Paper Information:
+            </li>
+
+            <li class="pdfpaperInfo">
+                <div class="colpdf col-1" data-label="Title:">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                    optio,</div>
+                <div class="colpdf" data-label="Paper Type:">Thesis</div>
+                <div class="colpdf" data-label="College:">CCS</div>
+                <div class="colpdf" data-label="Authors:">ms. meow meow</div>
+                <div class="pdfbtnCont">
+                    <button class="pdfBtn redBtn">Back</button>
+                    <button class="pdfBtn redBtn" id="bookmarkBtn">Bookmark</button>
+                    <button class="pdfBtn redBtn" id="citeBtn">Cite</button>
                 </div>
-                <br/>
-                <button type="submit"  class="btn btn-success">Submit</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
+            </li>
 
-        <div class="paperinfoblock">
-                <!-- AUTHORS
-                <div class="paperinfo">
-                        <span>Author(s): </span>
+            <div id="bookmarkModal" class="modal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+                    <span class="bkClose close">&times;</span>
+                    <div class="modalinfoCont">
+
+                        <h2>Add bookmark</h2>
+
+                        <form class="bookmarkInput">
+
+                            <div class="group">      
+                                <input class="inputInfo" type="text" required>
+                                <span class="highlight"></span>
+                                <span class="bar"></span>
+                                <label class="infoLabel">Bookmark Name</label>
+                            </div>
+
+                            <br>
+                            <br>
+
+                            <button class="redBtn">Add</button>
+                        </form>
+                    </div>
+                </div>
+            
+            </div>
+
+            <div id="citeModal" class="modal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+                    <span class="ctClose close">&times;</span>
+                    <div class="modalinfoCont">
+
+                        <h2>Cite this paper</h2>
+                        add citation here
                         
-                        <span class="dispauthor"></span>
+                    </div>
                 </div>
-                -->
+            
+            </div>
+
         </div>
 
+        <div class="pdfdisplayCard">
 
-                <iframe  class="pdfviewer" src=""></iframe>
+            <iframe  class="pdfFrame" src=""></iframe>
 
+        </div>
+
+    </div>
 </div>
+
+<footer>
+<p>Silliman University Digital Repository</p>
+</footer>
+
+<script>
+
+    var bookmarkModal = document.getElementById("bookmarkModal");
+    var citeModal = document.getElementById("citeModal");
+
+    // Get the button that opens the modal
+    var bookmarkBtn = document.getElementById("bookmarkBtn");
+    var citeBtn = document.getElementById("citeBtn")
+
+    // Get the <span> element that closes the modal
+    var bkspan = document.getElementsByClassName("bkClose")[0];
+    var ctspan = document.getElementsByClassName("ctClose")[0];
+
+    // When the user clicks the button, open the modal 
+    bookmarkBtn.onclick = function() {
+        bookmarkModal.style.display = "block";
+    }
+
+    citeBtn.onclick = function() {
+        citeModal.style.display = "block"
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    bkspan.onclick = function() {
+        bookmarkModal.style.display = "none";
+    }
+
+    ctspan.onclick = function() {
+        citeModal.style.display = "none";
+    }
+
+</script>
 
 @endsection
