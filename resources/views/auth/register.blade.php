f6c91e3be31f7ab2a6cb97af879b5743af7155c5
DynOfficial <agirdeanne@yahoo.com>
2022-11-22 08:45:15 +0800

    Revertsregister

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,31 +1,22 @@
 @extends('layouts.auth')
-
 @section('content')
 <div class="indexCont">
 		<div class="indexWrapper">
-
 			<div class="registerCard">
-
 		        <img class="smallLogo" src="Img/SUDRSmallLogo.png">
    
                 <form class="formcontainerreg" method="POST" action="{{ route('register') }}">
                     @csrf
-
                         <div class="usertype">
-
                             <h4 id="typeheader">Please select a user type:</h4>
-
                                 
                                 <input id="facultytab" type="radio" name="UserType" value="Faculty" onclick="text(3)"/>
                                 <label id="facultylabel" class="uitab" for="facultytab">Faculty</label>
-
                                 <input id="orgtab" type="radio" name="UserType" value="Organization" onclick="text(2)"/>
                                 <label id="orglabel" class="uitab" for="orgtab">Organization</label>
-
                                 <input id="studenttab" type="radio" name="UserType"  value="Student" onclick="text(1)" />
                                 <label id="studentlabel"class="uitab" for="studenttab">Student</label>
                         </div>
-
                         <div class="typelayout">
                                 <section class="faculstud" id="student">
                                     <!-- USER NAME -->
@@ -38,7 +29,7 @@
                                     <!-- FIRST NAME -->
                                     <div class="reginput" >
                                         <i class="fa-solid fa-1"></i>
-                                        <input id="Fname" type="text" class="input" name="FirstName" value="{{ old('FirstName') }}" required autocomplete="Fname"
+                                        <input id="Fname" type="text" class="input" name="FirstName" value="{{ old('FirstName') }}" required autocomplete="Fname" 
                                         placeholder = "First Name" autofocus>   
                                     </div>
                                         @error('Fname')
@@ -58,21 +49,24 @@
                                                 <strong>{{ $message }}</strong>
                                             </span>
                                         @enderror
-
                                     <!-- LAST NAME -->
                                     <div class="reginput" >
                                             <i class="fa-solid fa-3"></i>
-                                            <input id="Lname" type="text" class="input" name="LastName" value="{{ old('LastName') }}" required autocomplete="Lname"
+                                            <input id="Lname" type="text" class="input" name="LastName" value="{{ old('LastName') }}" required autocomplete="Lname" 
                                             placeholder = "Last Name" autofocus>
                                     </div>
+                                    <!--
+                                        @error('studid')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror-->
 
                                     <!-- COLLEGE -->
                                     <div class="reginput" id="collegeinput">
                                         <i class="fa-solid fa-graduation-cap"></i>
-
                                         <select class="college style" name="college">
                                             <option value="blank">Select College</option>
-
                                             @foreach($College as $Colleges)
                                                 <option value="{{$Colleges->CollegeName}}">{{$Colleges->CollegeName}}</option>
                                             @endforeach
@@ -85,23 +79,23 @@
                                             </span>
                                         @enderror
                                 </section>    
-                            
-                                
-                                <!-- Organization Name -->
+
+
+
                                 <div class="regrequired">
+
                                     <section class="org" id="organization">
                                         <div class="reginput" >
                                             <i class="fa-solid fa-school"></i>
-                                            <input id="Orgname" type="text" class="input" name="OrganizationName" value="{{ old('OrganizationName') }}"   
-                                            placeholder = "Organization Name" required>   
+                                            <input id="Orgname" type="text" class="input" name="OrganizationName" value="{{ old('OrganizationName') }}"  
+                                            placeholder = "Organization Name">   
                                         </div>
-                                            @error('OrganizationName')
+                                            @error('name')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
                                                 </span>
                                             @enderror
                                     </section> 
-
                                     <!-- EMAIL -->
                                     <div class="reginput" >
                                             <i class="fa-solid fa-envelope"></i>
@@ -113,7 +107,6 @@
                                                 <strong>{{ $message }}</strong>
                                             </span>
                                         @enderror
-
                                     <!-- PASSWORD -->
                                     <div class="reginput">
                                             <i class="fa-solid fa-lock"></i>
@@ -125,20 +118,15 @@
                                                 <strong>{{ $message }}</strong>
                                             </span>
                                         @enderror
-
                                     
-
                                     <!-- CONFIRM PASSWORD -->
                                     <div class="reginput">
                                             <i class="fa-solid fa-square-check"></i>
                                             <input id="password-confirm" type="password" class="input" name="password_confirmation" required autocomplete="new-password"
                                             placeholder = "Confirm Password">
                                     </div>
-
                                 </div>
-
                         </div>
-
                         
                   
                         <!-- REGISTER -->
@@ -146,18 +134,15 @@
                             <button class="indexBtn">
                                 <a href = "{{ route('login') }}">Back</a>
                             </button>
-
                             <button type="submit" class="indexBtn">
                                 {{ __('Register') }}
                             </button>
                         </div>
-                       
-                        <script>
 
+                        <script>
                             document.getElementById('student').style.display = "none";
                             document.getElementById("collegeinput").style.display = "none";
                             document.getElementById("organization").style.display = "none";
-
                             function text(x){
                                 if ( x == 1){
                                     document.getElementById('student').style.display = "block";
@@ -166,7 +151,6 @@
                                 }
                                 else if ( x == 2){
                                     document.getElementById("student").style.display = "none";
-                                    document.getElementById("collegeinput").style.display = "none";
                                     document.getElementById("organization").style.display = "block";
                                 }
                                 else if ( x == 3){
@@ -177,13 +161,12 @@
                                 return; 
                             }
                         </script>
-                     </form>
-                
+
+                </form>
             </div>
         </div>
     </div>
     <footer>
 		<p>Silliman University Digital Repository</p>
 	</footer>
-
 @endsection
