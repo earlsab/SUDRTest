063bf39da8f13841fd7463b2a6be89971434ffff
ChuuniQ <kyleestabillo@su.edu.ph>
2022-11-23 23:22:31 +0800

    - removed delete button on maintain papers

--- resources/views/admin/updatepaper.blade.php
+++ resources/views/admin/updatepaper.blade.php
@@ -6,7 +6,6 @@
 			<div class="col col-2">Paper Type</div>
 			<div class="col col-3">College</div>
 			<div class="col col-4">View Link</div>
-			<div class="col col-4">Delete</div>
 		</li>
 		@foreach($paper as $papers)
 			@if($papers->UploaderUserID == ($user = \Auth::guard('web')->user()->UserID))
@@ -15,13 +14,6 @@
 			<div class="col col-2" data-label="Paper Type:">{{$papers->PaperType}}</div>
 			<div class="col col-3" data-label="College:">{{$papers->College}}</div>
 			<div class="col col-4" data-label="View Link:"><button class="redBtn" onclick="location.href='{{route('viewPDF', $papers->PaperID)}}'">View</button></div>
-			<div class="col col-5" data-label="View Link:">
-				<form method="POST" action="{{route('papers.destroy', $papers->PaperID) }}">
-						@csrf
-						@method('DELETE')
-						<button type="submit" class="redBtn" >Delete</button>
-				</form>
-			</div>
 		</li>
 			@endif		
 		@endforeach		
