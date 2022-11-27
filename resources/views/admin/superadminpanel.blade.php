37a5c10e3c10ee5e905404b4df9c518a19da241e
ChuuniQ <kyleestabillo@su.edu.ph>
2022-11-27 22:24:22 +0800

    - super admin ui

--- resources/views/admin/superadminpanel.blade.php
+++ resources/views/admin/superadminpanel.blade.php
@@ -49,10 +49,23 @@
 							<div class="col col-2" data-label="First Name:">{{$users->FirstName}}</div>
 							<div class="col col-3" data-label="Email:">{{$users->email}}</div>
 							<div class="col col-4" data-label="College:">{{$users->college}}</div>
-							<div class="col col-5" data-label="Role Change:"><button class="redBtn">Add</button></div>
+							<div class="col col-5" data-label="Role Change:"><button id="modalOneBtn" class="redBtn">Add</button></div>
 						</li>
 						@endforeach
 					</ul>
+
+					<div id="modalOne" class="modal">
+						<!-- Modal content -->
+						<div class="modal-content">
+							<span class="m1Close close">&times;</span>
+							<div class="modalinfoCont">
+
+								<h2>Chage Role</h2>
+								
+							</div>
+						</div>	  
+					</div>
+					
 				</div>
 
             </div>
@@ -63,5 +76,27 @@
 	<footer>
 		<p>Silliman University Digital Repository</p>
 	</footer>
+
+	<script>
+
+		var modalOne = document.getElementById("modalOne");
+
+		// Get the button that opens the modal
+		var modalOneBtn = document.getElementById("modalOneBtn");
+
+		// Get the <span> element that closes the modal
+		var m1span = document.getElementsByClassName("m1Close")[0];
+
+		// When the user clicks the button, open the modal 
+		modalOneBtn.onclick = function() {
+			modalOne.style.display = "block";
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		m1span.onclick = function() {
+			modalOne.style.display = "none";
+		}
+
+	</script>
 
 	@endsection
