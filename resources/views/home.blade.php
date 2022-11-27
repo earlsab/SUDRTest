fef8052bbe49748a6a9f83a5848c9c2f693d95f9
DynOfficial <agirdeanne@yahoo.com>
2022-11-27 15:46:45 +0800

    Lorem Something

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,7 +96,9 @@
 						<br>
 
 						<p>
-							tae
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 						</p>
 
 					</div>
@@ -190,7 +192,9 @@
 						<br>
 
 						<p>
-							tae
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 						</p>
 
 					</div>
